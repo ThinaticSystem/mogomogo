33dd519eecd411b23d0d72f2511f57110d9c3e05
ThinaticSystem <git@thinaticsystem.com>
2026-02-11 09:27:38 +0000

    refactor: Use function declaration

--- src/util/collection.ts
+++ src/util/collection.ts
@@ -1,9 +1,9 @@
 /** Creates an async generator that maps each element of a {@link collection} using an async {@link mapper} function */
-export const mapAsync = async function* <TSource, TMapped>(
+export async function* mapAsync<TSource, TMapped>(
 	collection: Iterable<TSource>,
 	mapper: (item: TSource) => Promise<TMapped>,
 ) {
 	for (const element of collection) {
 		yield await mapper(element);
 	}
-};
+}
