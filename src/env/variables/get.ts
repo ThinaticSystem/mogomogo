8f2efb6ab6682cc7370f800fa2675f7c19d2a352
ThinaticSystem <git@thinaticsystem.com>
2026-02-11 08:33:48 +0000

    feat: Unrecommend `create-sfu-app.ts`
    - **Change**: Make environment variables optional
    
      - `CLOUDFLARE_ACCOUNT_ID`
      - `CLOUDFLARE_CALLS_API_TOKEN`
    
    - **Feature**: Introduce `EnvironmentVariableValidators` class for flexible environment variable validation

--- src/env/variables/get.ts
+++ src/env/variables/get.ts
@@ -1,25 +1,20 @@
 import { memoize } from "../../util/function.ts";
-import { throws } from "../../util/throw.ts";
+import { ENVIRONMENT_VARIABLE_KEYS } from "./keys.ts";
 import { loadDotEnv } from "./load.ts";
-
-/**
- * Get required environment variable or throw
- * @throws If the environment variable is missing or empty
- */
-const require = (key: string) =>
-	(process.env[key] ?? throws(new Error(`Required environment variable "${key}" is missing`))) ||
-	throws(new Error(`Required environment variable "${key}" is empty`));
+import { EnvironmentVariableValidators } from "./validator.ts";
 
 /**
  * This function is memoized to ensure that the .env file is loaded only once
- * @throws If any required environment variable is missing or empty
+ * @throws If any value is invalid
  */
 export const getEnvironmentVariables = memoize(async () => {
 	await loadDotEnv();
 
+	const { optional } = new EnvironmentVariableValidators(process.env);
+	const KEYS = ENVIRONMENT_VARIABLE_KEYS;
 	return {
-		CLOUDFLARE_CALLS_API_TOKEN: require("CLOUDFLARE_CALLS_API_TOKEN"),
-		CLOUDFLARE_ACCOUNT_ID: require("CLOUDFLARE_ACCOUNT_ID"),
+		CLOUDFLARE_CALLS_API_TOKEN: optional(KEYS.CLOUDFLARE_CALLS_API_TOKEN),
+		CLOUDFLARE_ACCOUNT_ID: optional(KEYS.CLOUDFLARE_ACCOUNT_ID),
 	} as const;
 });
 export type EnvironmentVariables = Awaited<ReturnType<typeof getEnvironmentVariables>>;
