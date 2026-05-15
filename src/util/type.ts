38124b34e4389101dd6dde6aa6153d87b724b491
ThinaticSystem <git@thinaticsystem.com>
2026-05-15 13:26:52 +0000

    feat(util): `ReadonlyRecursive` type

--- src/util/type.ts
+++ src/util/type.ts
@@ -8,3 +8,27 @@ export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) ex
 ) => void
 	? I
 	: never;
+
+/**
+ * @example
+ * type Result = ReadonlyRecursive<{
+ *   a: string;
+ *   b: {
+ *     c: number;
+ *   };
+ * }>;
+ * //   ^? => {
+ * //     readonly a: string;
+ * //     readonly b: {
+ * //       readonly c: number;
+ * //     };
+ * //   }
+ * }
+ */
+export type ReadonlyRecursive<T> = T extends Function
+	? T
+	: T extends object
+		? {
+				readonly [K in keyof T]: ReadonlyRecursive<T[K]>;
+			}
+		: T;
